b1c111a22025761f210a5085c2f90b2cbbed2bc8
Baher <baher.hedayati@gmail.com>
2018-10-17 22:29:48 -0400

    delete trash

--- word 2 vec/01.save-tfidf-train-data.py
+++
@@ -1,6 +0,0 @@
-import pickle
-import tfidf1
-from sklearn.datasets import fetch_20newsgroups
-
-X_train = fetch_20newsgroups(subset='train' , remove=('headers', 'footers', 'quotes'))
-tfidf1.run(X_train.data, "train1"); # pass all documents in test set to tfidf
